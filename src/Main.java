a8fb15ca85bcf3022adce1fcf026f0d27ea7bdab
Yuumin97 <fiesportlmtc@gmail.com>
2022-07-27 23:13:41 +0700

    va

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap size cua ma tran: ");
+        int n = scanner.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Enter element (" + i + "," + j + ")");
+                matrix[i][j] = scanner.nextInt();
+                if (i == j) sum += matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%-5d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Tổng số phần tử trong đường chéo chính là " + sum);
+
+    }
+}
